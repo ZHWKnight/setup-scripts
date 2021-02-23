97b43595017ee8e9a086bc4a995973aad8d273c2
ZHWKnight <zhwknight@foxmail.com>
2021-02-23 17:59:14 +0800

    Add a python script.

---
+++ include/python/new_version_source_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script will setup PPA source to install new version python.
+#
+# Github: https://github.com/zhwknight/setup-scripts
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
