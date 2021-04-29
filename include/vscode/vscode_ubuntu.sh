3a2de089423874af4b1f4b4ed5be8a63b1515d55
ZHWKnight <zhwknight@foxmail.com>
2021-04-29 16:19:18 +0800

    Add sys max fs watches function.

--- include/vscode/vscode_ubuntu.sh
+++ include/vscode/vscode_ubuntu.sh
@@ -8,3 +8,6 @@ sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key BC528686
 sudo sh -c 'echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install code
+
+sudo sh -c 'echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf'
+sudo sysctl -p
