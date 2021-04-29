9838d31b9da3a2246f9afecda6a81d75756ec381
ZHWKnight <zhwknight@foxmail.com>
2021-04-29 16:22:02 +0800

    Add webots install script.

---
+++ include/webot/webots_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will install "Webots".
+#
+# Github: https://github.com/zhwknight/setup-scripts
+
+sudo wget -qO- https://cyberbotics.com/Cyberbotics.asc | sudo apt-key add -
+sudo apt-add-repository 'deb https://cyberbotics.com/debian/ binary-amd64/'
+sudo apt update
+sudo apt-get install webots ros-melodic-webots-ros -y
+echo "export WEBOTS_HOME=/usr/local/webots" >>~/.bashrc
