15e2bb9d786e9f53fa93a94a52ff113901eab6c4
ZHWKnight <zhwknight@foxmail.com>
2021-04-29 16:21:50 +0800

    Add cmake install script.

---
+++ include/cmake/cmake_ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script will install "cmake".
+#
+# Github: https://github.com/zhwknight/setup-scripts
+
+sudo apt-get update
+sudo apt-get install \
+  apt-transport-https \
+  ca-certificates \
+  gnupg \
+  software-properties-common \
+  wget \
+  lsb-release \
+  ;
+
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+
+sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -sc) main"
+sudo apt-get update
+
+sudo apt-get install kitware-archive-keyring
+sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
