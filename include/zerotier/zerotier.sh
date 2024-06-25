5b1d58eb409a589f0374fd25e8e8e8aeb6e25194
ZHWKnight <zhwknight@foxmail.com>
2024-06-25 20:52:10 +0800

    Update zerotier install.sh

--- include/zerotier/zerotier.sh
+++ include/zerotier/zerotier.sh
@@ -4,11 +4,4 @@
 #
 # Github: https://github.com/zhwknight/setup-scripts
 
-# curl -s https://install.zerotier.com | sudo bash
-# sudo apt install -y gpg
-# curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import &&
-#     if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
-
-sudo apt-key adv --keyserver "hkp://keyserver.ubuntu.com:80" --recv-key 74A5E9C458E1A431F1DA57A71657198823E52A61
-sudo apt-add-repository "deb http://download.zerotier.com/debian/$(lsb_release -sc) $(lsb_release -sc) main"
-sudo apt install -y zerotier-one
+curl -s https://install.zerotier.com | sudo bash
